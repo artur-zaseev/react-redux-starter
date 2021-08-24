9d7e4f1695b6bc394937801d0558bed6db0104d2
Артур Засеев <37107439+artur-zaseev@users.noreply.github.com>
2021-08-24 21:06:55 +0300

    update reducer

--- src/redux/reducers/counter.js
+++ src/redux/reducers/counter.js
@@ -1,19 +1,15 @@
 import { INCREMENT, DECREMENT } from "../types/counter";
 
 const initState = {
-  count: 5,
+  count: 5
 };
 
 export default function reducer(state = initState, action) {
   switch (action.type) {
     case INCREMENT:
-      return {
-        count: state.count + 1,
-      };
+      return { ...state, count: state.count + 1 };
     case DECREMENT:
-      return {
-        count: state.count - 1,
-      };
+      return { ...state, count: state.count - 1 };
     default:
       return state;
   }
